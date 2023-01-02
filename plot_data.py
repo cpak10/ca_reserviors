e094084ebb0e218ea64cb2e9f98435c09d76124f
Chris Pak <christopher.a.pak@gmail.com>
2023-01-01 23:28:35 -0800

    Mod. labels in plot_data.py.

--- plot_data.py
+++ plot_data.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # filters
-reservoir = "SHA"
-reservoir_full_name = "Shasta Reservoir Last 90 Days"
+reservoir = "ORO"
+reservoir_full_name = "Oroville Reservoir Last 90 Days"
 date = "20230101"
 
 # file root
@@ -25,18 +25,19 @@ data_precip_filt = data_precip[data_precip["station_id"] == reservoir]
 fig, ax1 = plt.subplots()
 fig.set_size_inches(18.5, 10.5)
 ax2 = ax1.twinx()
-ax1.plot(data_reservoir_filt["date_f"], data_reservoir_filt["storage_change"], "-b")
+ax1.plot(data_reservoir_filt["date_f"], data_reservoir_filt["storage_change"], "-b", label = "Water Change")
 ax1.axhline(0, linestyle = "--", color = "r", alpha = .75)
 
 # plot the bar chart
-ax2.bar(data_precip_filt["date_f"], data_precip_filt["precip_24"], width = 0.9, alpha = .5)
+ax2.bar(data_precip_filt["date_f"], data_precip_filt["precip_24"], width = 0.9, alpha = .5, label = "Precipitation")
 
 # add in extra information
 ax1.set_ylabel("Water Change (Acre Feet)")
 ax2.set_ylabel("Precipitation (Inches)")
 ax1.set_title(reservoir_full_name)
-ax1.legend(["Water Change"], loc = 2)
-ax2.legend(["Precipitation"], loc = 3)
+lines, labels = ax1.get_legend_handles_labels()
+lines2, labels2 = ax2.get_legend_handles_labels()
+ax2.legend(lines + lines2, labels + labels2, loc = 2)
 
 # show the plot
 plt.savefig(f"{file_root}\\outputs\\{reservoir_full_name}_{date}.png", dpi = 300)
